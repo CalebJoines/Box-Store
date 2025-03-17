22655e3bc7bc4cb2d1b5f093fcb6f7f5123aa15c
CalebJoines <calebjoines@gmail.com>
2025-03-17 14:45:38 -0400

    Set up initial User input for selling or buying an item.

--- WQSJoinesHoustonWalker.java
+++ WQSJoinesHoustonWalker.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class WQSJoinesHoustonWalker {
     public static void main(String[] args) {
+        System.out.println("Welcome to Wilmington Quickstop.\n Would you like to sell an item?(1) Or add an item?(2):");
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        switch (input) {
+            //If we chose to sell an Item
+            case 1: {
 
+            }
+            //If we chose to buy an item
+            case 2: {
+            }
+            default :
+                System.out.println("Invalid selection. Please enter 1 or 2.");
+                break;
+        }
     }
 }
