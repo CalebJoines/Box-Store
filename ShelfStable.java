6619533116560c9c3b3e9145ba0c618be3218ac0
LandonTheCoder <100165458+LandonTheCoder@users.noreply.github.com>
2025-03-17 16:31:28 -0400

    Change perishability default for ShelfStable ShelfStable should be non-perishable (it is called "shelf stable" after all).

--- ShelfStable.java
+++ ShelfStable.java
@@ -1,8 +1,12 @@
 public class ShelfStable extends FoodItem {
     public ShelfStable(String name, String description, String brand, double price, String returnPolicy, String expirationDate, boolean isPerishable) {
         super(name, description, brand, price, returnPolicy, expirationDate, isPerishable);
+	// ShelfStable is non-perishable
+	setPerishable(false);
     }
     public ShelfStable(String name, double price, String expirationDate) {
         super(name, price, expirationDate);
+	// ShelfStable is non-perishable
+	setPerishable(false);
     }
 }
