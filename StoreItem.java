1eaace5bc63609264f6f50658bdda8cbfe2c03fe
CalebJoines <calebjoines@gmail.com>
2025-03-17 13:26:09 -0400

    Added all private vars and getters.

--- StoreItem.java
+++ StoreItem.java
@@ -1,6 +1,19 @@
 public class StoreItem {
-    private double test;
-    public StoreItem() {
-
+    private double price;
+    private String brand;
+    private String name;
+    private String description;
+    private String returnPolicy;
+    public StoreItem(String name, String description, String brand, double price, String returnPolicy){
+        this.name = name;
+        this.description = description;
+        this.brand = brand;
+        this.price = price;
+        this.returnPolicy = returnPolicy;
     }
+    public String getName(){return this.name;}
+    public double getPrice(){return this.price;}
+    public String getBrand(){return this.brand;}
+    public String getDescription(){return this.description;}
+    public String getReturnPolicy(){return this.returnPolicy;}
 }
