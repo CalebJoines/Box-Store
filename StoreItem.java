9847fdebc71bd577553ff0791221036f68f3d444
LandonTheCoder <100165458+LandonTheCoder@users.noreply.github.com>
2025-03-17 13:29:18 -0400

    Add setters This adds setters (and makes some minor code style changes).

--- StoreItem.java
+++ StoreItem.java
@@ -4,16 +4,19 @@ public class StoreItem {
     private String name;
     private String description;
     private String returnPolicy;
-    public StoreItem(String name, String description, String brand, double price, String returnPolicy){
+    public StoreItem(String name, String description, String brand, double price, String returnPolicy) {
         this.name = name;
         this.description = description;
         this.brand = brand;
         this.price = price;
         this.returnPolicy = returnPolicy;
     }
-    public String getName(){return this.name;}
-    public double getPrice(){return this.price;}
-    public String getBrand(){return this.brand;}
-    public String getDescription(){return this.description;}
-    public String getReturnPolicy(){return this.returnPolicy;}
+    public String getName() {return this.name;}
+    public double getPrice() {return this.price;}
+    public void setPrice(double price) {this.price = price;}
+    public String getBrand() {return this.brand;}
+    public String getDescription() {return this.description;}
+    public void setDescription(String description) {this.description = description;}
+    public String getReturnPolicy() {return this.returnPolicy;}
+    public void setReturnPolicy(String returnPolicy) {this.returnPolicy = returnPolicy;}
 }
