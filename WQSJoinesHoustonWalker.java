37af8592b7327815030bd384a22157d3ccc13991
CalebJoines <calebjoines@gmail.com>
2025-03-18 14:12:09 -0400

    Created menu for adding new items or creating existing ones.

--- WQSJoinesHoustonWalker.java
+++ WQSJoinesHoustonWalker.java
@@ -13,10 +13,24 @@ public class WQSJoinesHoustonWalker {
         ArrayList<ElectronicsItem> electronicsArray = new ArrayList<ElectronicsItem>();
         ArrayList<ClothingItem> clothingArray = new ArrayList<ClothingItem>();
 
+        // Adding CleaningSupply objects (AI generated)
+        householdArray.add(new CleaningSupply("Glass Cleaner", "Cleans windows", "Windex", 5.99, "30 days", "Spray", "Wipe with cloth"));
+        householdArray.add(new CleaningSupply("Dish Soap", "Cuts grease", "Dawn", 3.49, "No returns", "Pour", "Scrub with sponge"));
+        householdArray.add(new CleaningSupply("Floor Cleaner", "Removes stains", "Pine-Sol", 6.99, "14 days", "Mop", "Dilute in water"));
+        householdArray.add(new CleaningSupply("Bathroom Spray", "Kills germs", "Lysol", 7.99, "30 days", "Spray", "Wipe with towel"));
+
+        // Adding Furniture objects (AI Generated)
+        householdArray.add(new Furniture("Office Chair", "Ergonomic chair", "IKEA", 99.99, "60 days", "Leather"));
+        householdArray.add(new Furniture("Dining Table", "Wooden dining table", "Ashley", 199.99, "90 days", "Oak Wood"));
+        householdArray.add(new Furniture("Sofa", "Comfortable 3-seater", "La-Z-Boy", 499.99, "120 days", "Fabric"));
+        householdArray.add(new Furniture("Bed Frame", "Queen size frame", "Wayfair", 299.99, "45 days", "Metal"));
+
+
         switch (input) {
             //If we chose to sell an Item
             case 1: {
-
+                System.out.println("Test");
+                break;
             }
             //If adding to inventory:
             //Asks what type of item (Food, Electronics, Clothing, or Household).
@@ -25,8 +39,8 @@ public class WQSJoinesHoustonWalker {
             //Continue looping until the user confirms they are done adding items.
             //Displays the updated inventory for the item(s) added.
             case 2: {
-                boolean condition = false;
-                do {
+                boolean condition = true;
+                while (condition) {
                     condition = false;
                     System.out.println("Enter which type of item you wish for (Food (1), Electronics (2), Clothing (3), or Household (4):");
                     int typeOfItem = sc.nextInt();
@@ -47,21 +61,29 @@ public class WQSJoinesHoustonWalker {
                         }
                         case 4: {
                             displayHouseholdItems(householdArray);
+                            System.out.println("Would you like to add more of an existing item?(1) Or Create a new item?(2)");
+                            int userInput = sc.nextInt();
+                            if (userInput == 1){
+                                //add existing item method
+                            }
+                            else{
+                                //create new item method
+                            }
                             break;
                         }
-
                         default:
                             System.out.println("");
                             condition = true;
                             break;
                     }
-                } while (condition);
-
+                }
+                break;
             }
-            default :
+            default:
                 System.out.println("Invalid selection. Please enter 1 or 2.");
                 break;
         }
+
     }
 
     public static void displayHouseholdItems(ArrayList<HouseholdItem> items) {
@@ -75,7 +97,7 @@ public class WQSJoinesHoustonWalker {
 
             if (item instanceof CleaningSupply) {
                 CleaningSupply cs = (CleaningSupply) item;
-                type = "Cleaning Supply";
+                type = "Application";
                 additionalInfo = "Use: " + cs.getUseMethod() + ", Apply: " + cs.getApplicationMethod();
             } else if (item instanceof Furniture) {
                 Furniture f = (Furniture) item;
